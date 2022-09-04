18dbcfe37eb0ed456efa98563b95583c51a9a581
Francisco Carmo <franciscoacarmo@gmail.com>
2022-09-04 10:58:46 -0300

    implementa inicialmente os requisitos básicos

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { MongoClient } from "mongodb";
+import mongodb from "mongodb";
+
 import dotenv from "dotenv";
 import joi from "joi";
+import dayjs from "dayjs";
 
 dotenv.config();
 
@@ -10,14 +13,14 @@ app.use(express.json());
 
 const userSchema = joi.object({
   name: joi.string().required(),
-  age: joi.number().required(),
 });
 
-const validation = userSchema.validate(user, { abortEarly: true });
+const messageSchema = joi.object({
+  to: joi.string().required(),
+  text: joi.string().required(),
+  type: joi.string().valid("message", "private_message").required(),
+});
 
-if (validation.error) {
-  console.log(validation.error.details);
-}
 // conectando ao banco
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
@@ -26,23 +29,135 @@ mongoClient.connect().then(() => {
   db = mongoClient.db("test");
 });
 
-// app.get("/usuarios", (req, res) => {
-//   // buscando usuários
-//   db.collection("users")
-//     .find()
-//     .toArray()
-//     .then((users) => {
-//       console.log(users); // array de usuários
-//     });
-// });
+async function removeUser() {
+  const participants = await db.collection("participants").find({}).toArray();
+  console.log("Chamou a funcao de remover");
+  await console.log(participants);
 
-// app.post("/usuarios", (req, res) => {
-//   // inserindo usuário
-//   db.collection("users").insertOne({
-//     email: "joao@email.com",
-//     password: "minha_super_senha",
-//   });
-// });
+  for (let i = 0; i < participants.length; i++) {
+    const participant = participants[i];
+
+    let timeSinceLastStatus = Date.now() - participant.lastStatus;
+    console.log(participant.name + " - " + timeSinceLastStatus);
+
+    if (timeSinceLastStatus > 10000) {
+      await db
+        .collection("participants")
+        .deleteOne({ _id: new mongodb.ObjectId(participant._id) });
+
+      const messageToBeSaved = {
+        from: participant.name,
+        time: dayjs(new Date()).format("HH-mm-ss"),
+        text: "sai da sala ...",
+        to: "Todos",
+        type: "status",
+      };
+
+      await db.collection("messages").insertOne({ ...messageToBeSaved });
+      console.log(`removeu ${participant.name}`);
+    }
+  }
+}
+app.post("/participants", async (req, res) => {
+  // inserindo usuário
+
+  const user = req.body;
+  console.log(user);
+
+  const validation = userSchema.validate(user, { abortEarly: true });
+
+  if (validation.error) {
+    console.log(validation.error.details.map((x) => x.message));
+    res.status(422).send({});
+  } else if (await db.collection("participants").findOne({ name: user.name })) {
+    res.status(409).send("nome repetido");
+  } else {
+    db.collection("participants").insertOne({
+      lastStatus: Date.now(),
+      ...user,
+    });
+    res.status(201).send("OK");
+  }
+});
+
+app.get("/participants", async (req, res) => {
+  const participants = await db.collection("participants").find({}).toArray();
+  res.status(200).send(participants);
+});
+
+app.post("/messages", async (req, res) => {
+  // inserindo usuário
+
+  const message = req.body;
+  const from = req.headers.user;
+
+  const validation = messageSchema.validate(message, { abortEarly: true });
+  const hasName = await db.collection("participants").findOne({ name: from });
+  console.log(hasName);
+
+  const messageToBeSaved = {
+    from: from,
+    time: dayjs(new Date()).format("HH-mm-ss"),
+    ...message,
+  };
+  console.log(messageToBeSaved);
+
+  if (validation.error) {
+    console.log(validation.error.details.map((x) => x.message));
+    res.status(422).send({});
+  } else if (!hasName) {
+    res.status(422).send("nome não está presente na lista");
+  } else {
+    db.collection("messages").insertOne({ ...messageToBeSaved });
+    res.status(201).send("OK");
+  }
+});
+
+app.get("/messages", async (req, res) => {
+  const messages = await db.collection("messages").find({}).toArray();
+  const limit = parseInt(req.query.limit);
+  const user = req.headers.user;
+
+  const filteredMessages = messages.filter((x) => {
+    return (
+      x.from === user ||
+      x.to === user ||
+      x.type === "message" ||
+      x.type === "status"
+    );
+  });
+
+  if (filteredMessages.length <= limit) {
+    res.status(200).send(filteredMessages);
+  } else {
+    const newMessage = [];
+    for (let i = filteredMessages.length - 1; i >= 0; i--) {
+      newMessage.unshift(filteredMessages[i]);
+      if (newMessage.length === limit) break;
+    }
+    res.status(200).send(newMessage);
+  }
+});
+
+app.post("/status", async (req, res) => {
+  const user = req.headers.user;
+  const hasUser = await db.collection("participants").findOne({ name: user });
+
+  const updateDoc = {
+    $set: {
+      lastStatus: Date.now(),
+    },
+  };
+
+  if (!hasUser) {
+    res.status(404).send();
+  } else {
+    db.collection("participants").updateOne({ name: user }, updateDoc);
+    res.status(200).send();
+  }
+});
+
+setInterval(removeUser, 15000);
 
 app.listen(5000, () => {
   console.log("listening on port 5000");
